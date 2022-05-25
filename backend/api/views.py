2dd737321f85b3a39f091c7baf46642b2cf0697b
RahulGopathi <gopathi.1@iitj.ac.in>
2022-05-25 22:42:18 +0530

    Add API routes for cadidates

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from api.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from api.serializer import MyTokenObtainPairSerializer, RegisterSerializer, CandidateSerializer, IndividualCandidateSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
-
-# Create your views here.
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from main.models import Candidate
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -22,8 +21,36 @@ class RegisterView(generics.CreateAPIView):
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        '/api/token/',
+        '/api/login/',
         '/api/register/',
-        '/api/token/refresh/'
+        '/api/login/refresh/'
+        '/api/candidates/',
+        '/api/candidates/register',
+        '/api/candidates/id',
+        '/api/candidates/id/delete',
     ]
     return Response(routes)
+
+
+class CandidatesView(generics.ListAPIView):
+    queryset = Candidate.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CandidateSerializer
+
+
+class CandidateRegisterView(generics.CreateAPIView):
+    queryset = Candidate.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = IndividualCandidateSerializer
+
+
+class CandidateUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Candidate.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = IndividualCandidateSerializer
+
+
+class CandidateDeleteView(generics.DestroyAPIView):
+    queryset = Candidate.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = IndividualCandidateSerializer
